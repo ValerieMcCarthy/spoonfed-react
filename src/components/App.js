69797806483a70a86bfe1a5e9e47ca67e76fd02f
Ashley Robinson <ashleyhowerobinson@gmail.com>
2017-02-18 17:07:32 -0500

    tachyons booted up

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from './NavBar'
 import Errors from './Errors'
+import '../css/tachyons.css'
 
 export default class App extends Component {
   render(){
